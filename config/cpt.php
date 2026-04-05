567e3a32edf0948849407f0b99b1682f021ac4ef
MD. Musharaf Hossain <71216867+musharaf377@users.noreply.github.com>
2026-04-05 09:38:20 +0600

    Project custom post type create

--- config/cpt.php
+++ config/cpt.php
@@ -36,5 +36,42 @@ return array(
             "show_in_rest" => true,
             'query_var' => true
         )
+    ],
+    
+    // Projects Post type
+    [
+        'post_type' => 'projects',
+        'args' => array(
+            'label' => esc_html__('Projects', 'drilllcorp-core'),
+            'description' => esc_html__('Projects', 'drilllcorp-core'),
+            'labels' => array(
+                'name' => esc_html_x('Projects', 'Post Type General Name', 'drilllcorp-core'),
+                'singular_name' => esc_html_x('Project', 'Post Type Singular Name', 'drilllcorp-core'),
+                'menu_name' => esc_html__('Projects', 'drilllcorp-core'),
+                'all_items' => esc_html__('All Projects', 'drilllcorp-core'),
+                'view_item' => esc_html__('View Project', 'drilllcorp-core'),
+                'add_new_item' => esc_html__('Add New Project', 'drilllcorp-core'),
+                'add_new' => esc_html__('Add New Project', 'drilllcorp-core'),
+                'edit_item' => esc_html__('Edit Project', 'drilllcorp-core'),
+                'update_item' => esc_html__('Update Project', 'drilllcorp-core'),
+                'search_items' => esc_html__('Search Projects', 'drilllcorp-core'),
+                'not_found' => esc_html__('Not Found', 'drilllcorp-core'),
+                'not_found_in_trash' => esc_html__('Not found in Trash', 'drilllcorp-core'),
+                'featured_image' => esc_html__('Project Image', 'drilllcorp-core'),
+                'remove_featured_image' => esc_html__('Remove Project Image', 'drilllcorp-core'),
+                'set_featured_image' => esc_html__('Set Project Image', 'drilllcorp-core'),
+            ),
+            'supports' => array('title', 'thumbnail', 'excerpt', 'editor', 'comments'),
+            'taxonomies' => array('post_tag'), // this is IMPORTANT
+            'hierarchical' => false,
+            'public' => true,
+            "publicly_queryable" => true,
+            'show_ui' => true,
+            "rewrite" => array('slug' => 'projects', 'with_front' => true),
+            'can_export' => true,
+            'capability_type' => 'post',
+            "show_in_rest" => true,
+            'query_var' => true
+        )
     ]
 );
--- config/custom-taxonomy.php
+++ config/custom-taxonomy.php
@@ -24,4 +24,28 @@ return array(
             "show_in_quick_edit" => true,
         )
     ),
+    // Projects Category Taxonomy
+    array(
+        'taxonomy' => 'projects_cat',
+        'object_type' => 'projects',
+        'args' => array(
+            "labels" => array(
+                "name" => esc_html__("Projects Category", 'drilllcorp-core'),
+                "singular_name" => esc_html__("Projects Category", 'drilllcorp-core'),
+                "menu_name" => esc_html__("Projects Category", 'drilllcorp-core'),
+                "all_items" => esc_html__("All Projects Category", 'drilllcorp-core'),
+                "add_new_item" => esc_html__("Add New Projects Category", 'drilllcorp-core')
+            ),
+            "public" => true,
+            "hierarchical" => true,
+            "show_ui" => true,
+            "show_in_menu" => true,
+            "show_in_nav_menus" => true,
+            "query_var" => true,
+            "rewrite" => array('slug' => 'projects_cat', 'with_front' => true),
+            "show_admin_column" => true,
+            "show_in_rest" => true,
+            "show_in_quick_edit" => true,
+        )
+    ),
 );
